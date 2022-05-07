6b8ff1af8acb14b4cb330a03d8cd873c4dc83742
Polar502 <jaritar@miumg.edu.gt>
2022-05-07 10:46:23 -0600

    Commit Inicial

---
+++ hash.js
@@ -0,0 +1,60 @@
+/*ñ
+Tabla hash en javascrip 
+*/
+
+//Creacción de la tabla contendedora de la llaves y valores
+class TablaHash {
+    constructor(){
+        //Tabla con un tamaño de 100 inicializado en 0 
+        this.tabla = new Array(100); 
+        //tamaño inicial de la tabla
+        this.tamano = 0;
+    }
+    
+    //Recorrer la cadena para obtener el codigo del caractere en ascii
+    Thash(llave) {
+        //Dejamos hash en o 
+        let hash = 0;
+        // iniciando i en 0; hacer cuando i sea menor a la longitud de la llave; incrementar i
+        for (let i = 0; i < llave.length; i++){
+            //hash obtiene el codigo ascii de la llave y se van sumando los nuevos resultados de la derecha 
+            hash += llave.charCodeAt(i);
+        }
+        //Retronamos hash con los codigos de carateres 
+        return hash % this.tabla.length;
+    }
+   
+    //Declaramos la instrucciones de set 
+    set(llave, valor) {
+        //el indice cuyo valor es la suma de todos los codigos de caracteres de la llave
+        const indice = this.Thash(llave);
+        //llave y valor se guardaran en la tabla con indice "n" 
+        this.tabla[indice] = [llave, valor];
+        //el tamaño de la tabla se incrementa
+        this.tamano++;
+    }
+   
+    //Declaramos la instrucciones de get
+    get(llave) {
+        //Buscar con el valor de llave devuelto de Thash
+        const objetivo = this.Thash(llave);
+        //Retornamos los datos en la tabla
+        return this.tabla[objetivo];
+    }
+    
+}
+
+//Iniciamos creando la tabla contenedora
+const ht = new TablaHash();
+
+//Colocamos la llave y el valor con la función set previamente declarada
+ht.set("Tiros a puerta", 11);
+ht.set("Remates", "Rodrigo");
+ht.set("2", 5);
+ht.set("Fuera juego", 27);
+
+//Mostramos en consola la llave y el valor con la función get previamente declarada
+console.log(ht.get("Tiros a puerta"));
+console.log(ht.get("Remates"));
+console.log(ht.get("2"));
+console.log(ht.get("Fuera juego"));
